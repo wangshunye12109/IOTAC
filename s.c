df61172f4c1fb8da43fea64fb5e77ac23270c081
wangshunye12109 <30544001+wangshunye12109@users.noreply.github.com>
2019-07-02 21:01:19 +0800

    Update s.c

--- s.c
+++ s.c
@@ -10,7 +10,14 @@
 #include "wsy.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    // insert code here...     
+  char *wday[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  time_t t;
+  struct tm *p;
+  time(&t);
+  p = localtime(&t);
+  printf ("%d/%02d/%02d ", (1900+p->tm_year), (1+p->tm_mon), p->tm_mday);
+  printf("%s %02d:%02d:%02d\n", wday[p->tm_wday], p->tm_hour, p->tm_min, p->tm_sec);
 	FILE  *s = fopen(argv[1], "r");
 	send_(s, "127.0.0.1", 12345);
 	fclose(s);
